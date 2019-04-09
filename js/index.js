8334dabaaf908e65ecb921908752e6fc9630220f
mdelzotto <madelzotto@comcast.net>
2019-04-09 19:45:37 -0400

    Added a unique listener with a border around header img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const image1 = document.querySelector('.img1');
+  image1.addEventListener('mouseover', event => { // 1. mouseenter
+    image1.style.border = "2px solid red";
+    image1.classlist
+  })
